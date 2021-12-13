4ca718becf9428b40c060cd2a90e5f5bac4570ae
EricaReis <ericareis67@outlook.com>
2021-12-13 17:05:39 -0300

    update procfile

--- src/app.js
+++ src/app.js
@@ -4,4 +4,4 @@ const app = require('./index');
 const mongoose_connection = require('./config/mongoose');
 
 mongoose_connection(process.env.MONGO_CONNECTION);
-app.listen(port, () => {console.log('Servidor rodando na porta', process.env.PORT || port)});
+app.listen(process.env.PORT || port, () => {console.log('Servidor rodando na porta', process.env.PORT || port)});
